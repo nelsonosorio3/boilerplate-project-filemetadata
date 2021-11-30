f4e82c35f6d43cec7414839c2d11106fc2aea45b
Nelson Alejandro Osorio Ramirez <nelsonsorio@hotmail.com>
2021-11-30 16:03:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/"});
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: "false"}));
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"),(req, res)=>{
+  const file = req.file;
+  console.log(file);
+  let name = file.originalname;
+  let type = file.mimetype;
+  let size = file.size;
+  res.json({name, type, size})
+});
 
 
 
